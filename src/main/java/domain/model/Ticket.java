08b1e241e8a00e835cf3080946839b684fcfe1b1
zapisator <zapisator@logan-club.ru>
2023-03-14 12:21:21 -0300

    Изменение модели, так как стал досупен правильный файл

--- src/main/java/domain/model/Ticket.java
+++ src/main/java/domain/model/Ticket.java
@@ -1,19 +1,42 @@
 package domain.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Ticket {
-    private String departureCity;
-    private String arrivalCity;
-    private LocalDateTime departureDate;
-    private LocalDateTime arrivalDate;
+    private String origin;
+    @JsonProperty("origin_name")
+    private String originName;
+    private String destination;
+    @JsonProperty("destination_name")
+    private String destinationName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    @JsonProperty("departure_date")
+    private LocalDate departureDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    @JsonProperty("departure_time")
+    private LocalTime departureTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yy")
+    @JsonProperty("arrival_date")
+    private LocalDate arrivalDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "H:mm")
+    @JsonProperty("arrival_time")
+    private LocalTime arrivalTime;
+    private String carrier;
+    private Integer stops;
+    private Double price;
 }
---
+++ src/main/java/domain/model/Tickets.java
@@ -0,0 +1,14 @@
+package domain.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tickets {
+    List<Ticket> tickets;
+}
