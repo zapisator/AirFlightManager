42ad55ecb40fb54e3eb8275def6fbb5668018937
zapisator <zapisator@logan-club.ru>
2023-03-14 12:32:40 -0300

    Так как изменилась модель, изменил чтение из файла

--- src/main/java/domain/service/TicketReader.java
+++ src/main/java/domain/service/TicketReader.java
@@ -1,26 +1,20 @@
 package domain.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import domain.model.Ticket;
+import domain.model.Tickets;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class TicketReader {
 
-    public List<Ticket> readTickets(String filename) throws IOException {
+    public Tickets readTickets(String filename) throws IOException {
         final String jsonString = Files.readString(Path.of(filename));
-        final ObjectMapper mapper = new ObjectMapper();
-        final CollectionType listType = mapper
-                .getTypeFactory()
-                .constructCollectionType(List.class, Ticket.class);
 
-        mapper.registerModule(new JavaTimeModule());
-        return mapper.readValue(jsonString, listType);
+        return new ObjectMapper()
+                .findAndRegisterModules()
+                .readValue(jsonString, Tickets.class);
     }
 
 }
