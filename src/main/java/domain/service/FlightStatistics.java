ae312b4718fb5f348abb238fff2967d8bb353b38
zapisator <zapisator@logan-club.ru>
2023-03-14 12:31:13 -0300

    Изменил способ расчёта статистик и чуть упростил вычисления.

--- src/main/java/domain/service/FlightStatistics.java
+++ src/main/java/domain/service/FlightStatistics.java
@@ -1,47 +1,49 @@
 package domain.service;
 
-import domain.model.Ticket;
+import domain.value.TicketValue;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.LongSummaryStatistics;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FlightStatistics {
 
     private final TimeZoneResolver zoneResolver = new TimeZoneResolverImpl();
     @Setter(AccessLevel.PRIVATE)
-    private LongSummaryStatistics statistics;
+    private DescriptiveStatistics statistics;
 
-    public static FlightStatistics create(List<Ticket> tickets, String departure, String arrival) {
+    public static FlightStatistics create(List<TicketValue> tickets, String departure, String arrival) {
         final FlightStatistics flightStatistics = new FlightStatistics();
-        final LongSummaryStatistics statistics = flightStatistics.statistics(tickets, departure, arrival);
+        final DescriptiveStatistics statistics = flightStatistics.statistics(tickets, departure, arrival);
 
         flightStatistics.setStatistics(statistics);
         return flightStatistics;
     }
 
     public double averageFlightTimeInMinutes() {
-        return statistics.getAverage();
+        return statistics.getMean();
     }
 
     public double percentile90FlightTime() {
-        return 0.9 * statistics.getCount();
+        return statistics.getPercentile(90);
     }
 
-    private LongSummaryStatistics statistics(List<Ticket> tickets, String origin, String destination) {
-        final LongSummaryStatistics statistics = new LongSummaryStatistics();
+    private DescriptiveStatistics statistics(List<TicketValue> tickets, String origin, String destination) {
+        final DescriptiveStatistics statistics = new DescriptiveStatistics();
+        final ZoneId departureZone = zoneResolver
+                .getTimeZoneForCity(origin);
+        final ZoneId arrivalZone = zoneResolver
+                .getTimeZoneForCity(destination);
 
         tickets.stream()
                 .filter(ticket -> isThisFlight(origin, destination, ticket))
                 .forEach(ticket -> {
-                             final ZoneId departureZone = zoneResolver.getTimeZoneForCity(origin);
-                             final ZoneId arrivalZone = zoneResolver.getTimeZoneForCity(destination);
                              final ZonedDateTime departure = ticket
                                      .getDepartureDate()
                                      .atZone(departureZone)
@@ -51,14 +53,13 @@ public class FlightStatistics {
                                      .atZone(arrivalZone)
                                      .withZoneSameInstant(ZoneId.of("UTC"));
                              final Duration flightTime = Duration.between(departure, arrival);
-
-                             statistics.accept(flightTime.toMinutes());
+                             statistics.addValue(flightTime.toMinutes());
                          }
                 );
         return statistics;
     }
 
-    private boolean isThisFlight(String origin, String destination, Ticket ticket) {
+    private boolean isThisFlight(String origin, String destination, TicketValue ticket) {
         return ticket.getDepartureCity().equals(origin)
                 && ticket.getArrivalCity().equals(destination);
     }
