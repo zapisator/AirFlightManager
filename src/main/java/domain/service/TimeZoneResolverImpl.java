396ee4456e8cd433dcbb16025cccc8acb836ec3b
zapisator <zapisator@logan-club.ru>
2024-07-30 02:44:24 -0300

    Заказчик сказал, что все данные в одном часовом поясе указаны. Учел

--- src/main/java/domain/service/TimeZoneResolverImpl.java
+++ src/main/java/domain/service/TimeZoneResolverImpl.java
@@ -5,13 +5,16 @@ import java.time.ZoneId;
 public class TimeZoneResolverImpl implements TimeZoneResolver {
 
     public ZoneId getTimeZoneForCity(String cityName) {
+        // cityName is ignored because of that
+        // afterward the company declared that all the time is of the same time zone
+        final String standardCitiName = "Vladivostok";
         return ZoneId.getAvailableZoneIds().stream()
                 .map(ZoneId::of)
                 .filter(
                         zoneId -> zoneId
                                 .toString()
                                 .toLowerCase()
-                                .contains(cityName.toLowerCase())
+                                .contains(standardCitiName.toLowerCase())
                 )
                 .findFirst()
                 .orElseThrow();
