a465a32c2f6528204d157362f28a7815cabf74d7
zapisator <zapisator@logan-club.ru>
2024-07-30 02:46:52 -0300

    Сделал более общим

--- src/main/java/domain/service/FlightStatistics.java
+++ src/main/java/domain/service/FlightStatistics.java
@@ -5,63 +5,61 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
-import java.time.Duration;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FlightStatistics {
 
-    private final TimeZoneResolver zoneResolver = new TimeZoneResolverImpl();
     @Setter(AccessLevel.PRIVATE)
     private DescriptiveStatistics statistics;
 
-    public static FlightStatistics create(List<TicketValue> tickets, String departure, String arrival) {
+    public static FlightStatistics create(
+            List<TicketValue> tickets,
+            Function<TicketValue, Double> statisticsPropertyExtractor,
+            Predicate<TicketValue> propertyFilter,
+            String departure,
+            String arrival
+    ) {
         final FlightStatistics flightStatistics = new FlightStatistics();
-        final DescriptiveStatistics statistics = flightStatistics.statistics(tickets, departure, arrival);
 
-        flightStatistics.setStatistics(statistics);
+        flightStatistics
+                .defineStatistics(
+                        tickets,
+                        statisticsPropertyExtractor, propertyFilter,
+                        departure, arrival
+                );
         return flightStatistics;
     }
 
-    public double averageFlightTimeInMinutes() {
+    public double average() {
         return statistics.getMean();
     }
 
-    public double percentile90FlightTime() {
-        return statistics.getPercentile(90);
+    public double minimum() {
+        return statistics.getMin();
     }
 
-    private DescriptiveStatistics statistics(List<TicketValue> tickets, String origin, String destination) {
-        final DescriptiveStatistics statistics = new DescriptiveStatistics();
-        final ZoneId departureZone = zoneResolver
-                .getTimeZoneForCity(origin);
-        final ZoneId arrivalZone = zoneResolver
-                .getTimeZoneForCity(destination);
+    public double percentile(double percentile) {
+        return statistics.getPercentile(percentile);
+    }
 
+    private void defineStatistics(
+            List<TicketValue> tickets,
+            Function<TicketValue, Double> propertyExtractor, Predicate<TicketValue> propertyFilter,
+            String origin, String destination
+    ) {
+        statistics = new DescriptiveStatistics();
+
+        statistics.setPercentileImpl(new Percentile()
+                .withEstimationType(Percentile.EstimationType.R_2)
+        );
         tickets.stream()
-                .filter(ticket -> isThisFlight(origin, destination, ticket))
-                .forEach(ticket -> {
-                             final ZonedDateTime departure = ticket
-                                     .getDepartureDate()
-                                     .atZone(departureZone)
-                                     .withZoneSameInstant(ZoneId.of("UTC"));
-                             final ZonedDateTime arrival = ticket
-                                     .getArrivalDate()
-                                     .atZone(arrivalZone)
-                                     .withZoneSameInstant(ZoneId.of("UTC"));
-                             final Duration flightTime = Duration.between(departure, arrival);
-                             statistics.addValue(flightTime.toMinutes());
-                         }
-                );
-        return statistics;
-    }
-
-    private boolean isThisFlight(String origin, String destination, TicketValue ticket) {
-        return ticket.getDepartureCity().equals(origin)
-                && ticket.getArrivalCity().equals(destination);
+                .filter(propertyFilter)
+                .forEach(ticket -> statistics.addValue(propertyExtractor.apply(ticket)));
     }
 
 }
