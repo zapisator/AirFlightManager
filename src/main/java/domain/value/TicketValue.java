568a2810d5b0ca089bbe0ad16075b784f453b328
zapisator <zapisator@logan-club.ru>
2024-07-30 02:43:47 -0300

    Добавил данных для расчетов

--- src/main/java/domain/value/TicketValue.java
+++ src/main/java/domain/value/TicketValue.java
@@ -1,16 +1,13 @@
 package domain.value;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
-@Getter
-public class TicketValue {
-    private final String departureCity;
-    private final String arrivalCity;
-
-    private final LocalDateTime departureDate;
-    private final LocalDateTime arrivalDate;
+public record TicketValue(
+        String departureCity,
+        String arrivalCity,
+        String carrier,
+        LocalDateTime departureDate,
+        LocalDateTime arrivalDate,
+        double price
+) {
 }
